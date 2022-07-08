54aa0fb0e801558143c6994d4ea6f44178e3879d
HARSHINIGOSU <harshinigosu711@gmail.com>
2022-07-08 15:32:36 +0530

    firstcommit

---
+++ Week13_Assignment5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Week13_Assignment5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

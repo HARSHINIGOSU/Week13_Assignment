7515abbf879b3be8b98a89e6430abe1cb747f13d
HARSHINIGOSU <harshinigosu711@gmail.com>
2022-07-08 19:37:18 +0530

    second commit

---
+++ Week13_Assignment5/Data/EmployeeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Week13_Assignment5.Models;
+
+namespace Week13_Assignment5.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext (DbContextOptions<EmployeeDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Week13_Assignment5.Models.Employee> Employee { get; set; }
+    }
+}
